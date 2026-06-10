792941aa04a5d0743b0df0bc686cc2658ef6e9cd
saurabh <saurabh20tiwari04@gmail.com>
2026-06-10 21:51:22 +0530

    password generator

---
+++ password-generator.py
@@ -0,0 +1,11 @@
+#student management task it will be able to do
+#1 :- Add a student 
+    #name 
+    #roll no.
+    #age
+    #cource
+#view all student 
+#search a student using the roll no or name 
+#update details like age and course 
+#remove a student record
+
